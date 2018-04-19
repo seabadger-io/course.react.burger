62197ca35cd6b1002f6f0ff24b84c337ff8dd9fa
Gabor Szoboszlai <itgipsy@itgipsy.com>
2018-04-19 14:07:55 +0000

    chore: Use CSS modules w local name

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -183,6 +183,8 @@ module.exports = {
                         importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
+                        modules: true,
+                        localIdentName: '[name]_[local][hash:base64:5]'
                       },
                     },
                     {
