bbbc5314cc320b971560a3e66090613a85f8e177
Gabor Szoboszlai <itgipsy@itgipsy.com>
2018-04-20 14:19:54 +0000

    feat: Add build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.css';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
+];
+
 const buildControls = (props) => {
   return (
-    <div>
-      
+    <div className={classes.BuildControls}>
+      {controls.map((ctrl) => {
+        return <BuildControl key={ctrl.label} label={ctrl.label} />;
+      })}
     </div>
-  )
+  );
 };
 
 export default buildControls;
