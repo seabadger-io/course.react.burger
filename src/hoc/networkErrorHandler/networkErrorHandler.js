a7faf515bcb023601ed30930982a6fde40f5def0
Gabor Szoboszlai <itgipsy@itgipsy.com>
2018-05-25 14:02:52 +0000

    feat: Implement network error handler HOC

---
+++ src/hoc/networkErrorHandler/networkErrorHandler.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Aux from '../Aux';
+import Modal from '../../components/UI/Modal/Modal';
+
+const networkErrorHandler = (WrappedComponent, axios) => {  
+  return class extends Component {
+    state = {
+      error: null
+    };
+
+    componentDidMount() {
+      axios.interceptors.response.use(response => response, (error) => {
+        this.setState({ error: error });
+      });
+      axios.interceptors.request.use((request) => {
+        this.setState({ error: null });
+        return request;
+      });
+    }
+
+    closeErrorMessage = () => {
+      this.setState({ error: null });
+    }
+  
+    render() {
+      return (
+        <Aux>
+          <Modal show={this.state.error} modalClosed={this.closeErrorMessage}>
+            {this.state.error ? this.state.error.message : ''}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      );
+    }
+  }
+}
+
+export default networkErrorHandler;
