24f5a95152c2c5e0663604376e827e4a0b5369aa
Gabor Szoboszlai <itgipsy@itgipsy.com>
2018-05-30 15:49:00 +0000

    feat: Add order list

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import classes from './Order.css';
 
-export default () => {
+export default (props) => {
+  const ingredients = props.data.ingredients;
+  if (!ingredients) return null;
+  const ingredientsStr = Object.keys(ingredients).map((i) => {
+    return i + ' (' + ingredients[i] + ')'; 
+  }).join(', ');
   return (
     <div className={classes.Order}>
-      <p>Ingredients</p>
-      <p>Price</p>
+      <p>Ingredients: {ingredientsStr}</p>
+      <p>Price: {props.data.price}</p>
     </div>
   )
 };
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import networkErrorHandler from '../../hoc/networkErrorHandler/networkErrorHandler';
 
-export default class Orders extends Component {
+class Orders extends Component {
+  state = {
+    loading: true,
+    orders: []
+  }
+
+  componentDidMount() {
+    axios.get('/orders.json')
+      .then((response) => {
+        const orders = [];
+        for (let key in response.data) {
+          orders.push({ id: key, ...response.data[key] });
+        }
+        this.setState({ orders: orders });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map((order) => <Order key={order.id} data={order} />)}
       </div>
     )
   }
 };
+
+export default networkErrorHandler(Orders, axios);
