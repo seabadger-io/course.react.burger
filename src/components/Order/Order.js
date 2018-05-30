3b8af8f423d86b39ffe6eea0a52172e609632aad
Gabor Szoboszlai <itgipsy@itgipsy.com>
2018-05-30 15:03:26 +0000

    feat: Add dummy orders component

---
+++ src/components/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Order.css';
+
+export default () => {
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients</p>
+      <p>Price</p>
+    </div>
+  )
+};
